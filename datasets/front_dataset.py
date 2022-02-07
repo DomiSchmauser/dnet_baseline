fix completion pipeline, fix nocs map calculation from occ grid

datasets/front_dataset.py
@@ -14,7 +14,7 @@ sys.path.append('..') #Hack add ROOT DIR
 from baseconfig import CONF
 
 from utils.data_utils import read_csv_mapping, load_hdf5, load_rgb, add_halfheight, coords2occupancy, get_voxel, boxpt2voxel
-from utils.pose_utils import backproject_rgb, cam2world, occ2noc
+from utils.pose_utils import backproject_rgb, cam2world, occ2noc, discretize_noc, occ2noc2
 from utils.net_utils import vg_crop
 from dvis import dvis
 
@@ -80,6 +80,8 @@ class Front_dataset(Dataset):
             # Dense Input
             record['dense_grid'] = coords2occupancy(record['sparse_coords'], padded_size=self.padded_size, as_padded_whl=True)
             record['obj_scan_mask'] = torch.clone(record['dense_grid'])
+            noc_shape = [3] + list(record['dense_grid'].shape)
+            record['noc_scan_mask'] = torch.zeros(noc_shape)
 
             if self.debugging_mode:
                 dvis(record['dense_grid'], c=0)
@@ -97,6 +99,7 @@ class Front_dataset(Dataset):
 
                     instance_id = int(anno['id']) + 2 # shift by 2 to avoid confusion 0 and 1 which represent occupancies
                     jid = anno['jid']
+                    #print(jid)
                     #cat_name = self.csv_dict[cat_id]
                     voxel_path = os.path.join(CONF.PATH.FUTURE3D, jid, 'model.binvox')
                     box_2d = anno['bbox']
@@ -117,9 +120,10 @@ class Front_dataset(Dataset):
 
                     # Use box 3d and occupancy values to index object scan mask
                     cropped_obj = vg_crop(record['dense_grid'].numpy(), box_3d)
+                    #noc_obj = occ2noc2(cropped_obj, rot_3d)
 
                     if self.debugging_mode:
-                        dvis(np.expand_dims(box_3d, axis=0), fmt='box')
+                        dvis(np.expand_dims(box_3d, axis=0), fmt='box', c=1)
                         #dvis(cropped_obj, c=1)
 
                     cropped_obj[cropped_obj != 0] = instance_id
@@ -128,8 +132,15 @@ class Front_dataset(Dataset):
                     # Load binvox
                     bin_vox = get_voxel(voxel_path, scale)
 
-                    # Binvox to noc
+                    # Binvox to noc, then discretize and scale, finally place in the scene
                     noc = occ2noc(bin_vox, rot_3d) # as pc
+
+                    noc = discretize_noc(noc, box_3d, quantization_size=self.quantization_size)
+
+
+                    record['noc_scan_mask'][int(box_3d[0]):int(box_3d[3]), int(box_3d[1]):int(box_3d[4]),
+                    int(box_3d[2]):int(box_3d[5])] = torch.from_numpy(cropped_obj)
+
                     '''
                     # Debugging
                     if self.debugging_mode:

models/BCompletionDec2.py
@@ -51,25 +51,25 @@ class BCompletionDec2(nn.Module):
         pred_compl = [[x_d0_crop > 0.0 for x_d0_crop in x_d0_crops] for x_d0_crops in bx_d0_crops]
         return pred_compl
 
-    def training_step(self, x_d2: torch.Tensor, x_e2: torch.Tensor, x_e1: torch.Tensor, bdscan, bbbox_lvl0: List, bgt_target: List):
+    def training_step(self, x_d2: torch.Tensor, x_e2: torch.Tensor, x_e1: torch.Tensor, rpn_gt, bbbox_lvl0: List, bgt_target: List):
         if self.gt_augm:
-            bgt_bbox, bgt_obj_inds = bdscan.bbboxes, bdscan.bobj_inds
+            bgt_bbox, bgt_obj_inds = rpn_gt['bbboxes'], rpn_gt['bobj_idxs']
             bbbox_lvl0 =  [cats(bbox_lvl0, gt_bbox,0)  for bbox_lvl0, gt_bbox in zip(bbbox_lvl0, bgt_bbox)]   
             bgt_target =  [gt_target + gt_obj_inds  for gt_target, gt_obj_inds in zip(bgt_target, bgt_obj_inds)]
         
         bx_d0_crops = self.forward(x_d2, x_e2, x_e1, bbbox_lvl0)
 
-        losses = self.loss(bx_d0_crops, bbbox_lvl0, bgt_target, bdscan)
+        losses = self.loss(bx_d0_crops, bbbox_lvl0, bgt_target, rpn_gt)
 
         return bx_d0_crops, losses, {}, {}
 
     
 
-    def loss(self, bx_d0_crops, bbbox_lvl0: List, bgt_target: List, bdscan):
+    def loss(self, bx_d0_crops, bbbox_lvl0: List, bgt_target: List, rpn_gt):
         bcompl_loss = []
         bweighted_loss = []
         for B in range(len(bbbox_lvl0)):
-            scan_inst_mask_crops = vg_crop(bdscan.bscan_inst_mask[B:B+1], bbbox_lvl0[B])
+            scan_inst_mask_crops = vg_crop(rpn_gt['bscan_inst_mask'][B:B+1], bbbox_lvl0[B])
             scan_compl_crops = [(scan_inst_mask_crop == int(bgt_target[B][i])).float() for i,scan_inst_mask_crop in enumerate(scan_inst_mask_crops)]
             
             # debugging

models/BSparseRPN_pure.py
@@ -48,14 +48,14 @@ class BSparseRPN_pure(nn.Module):
        
         return (bpred_bboxes, bgt_target, brpn_conf)
     
-    def _to_bboxes(self, bpred_reg_values, bdscan, min_conf, max_proposals):
+    def _to_bboxes(self, bpred_reg_values, rpn_gt, min_conf, max_proposals):
         # bdscan only used to get gt_targets
         bpred_bboxes = []
         brpn_conf = []
         bgt_target = []
 
         pred_coords_l, pred_reg_values_l = bpred_reg_values.decomposed_coordinates_and_features
-        # dvis(bdscan.breg_sparse.C[bdscan.breg_sparse.F[:,0]>0,1:] + bdscan.breg_sparse.F[bdscan.breg_sparse.F[:,0]>0,1:4].cpu())
+        #dvis(rpn_gt.breg_sparse.C[rpn_gt.breg_sparse.F[:,0]>0,1:] + rpn_gt.breg_sparse.F[rpn_gt.breg_sparse.F[:,0]>0,1:4].cpu())
         for B,(pred_coords, pred_reg_values) in enumerate(zip(pred_coords_l, pred_reg_values_l)):
             
             pred_logits = pred_reg_values[:,0:2]
@@ -102,9 +102,9 @@ class BSparseRPN_pure(nn.Module):
             pred_bboxes = torch.clamp(torch.cat([pred_bbox_centers - pred_bbox_sizes, pred_bbox_centers + pred_bbox_sizes], 1).round().int(), 0)
             #pred_bboxes[3:6] = torch.min(pred_bboxes[3:6], torch.Tensor([192, 96, 192]).cuda(), dim=1)
             #pred_bboxes[:, 3] = torch.clamp(pred_bboxes[:, 3],)
-            pred_bboxes[:, 3] = torch.clamp(pred_bboxes[:, 3], max=bdscan.bscan_shape[B][0]-1)
-            pred_bboxes[:, 4] = torch.clamp(pred_bboxes[:, 4], max=bdscan.bscan_shape[B][1]-1)
-            pred_bboxes[:, 5] = torch.clamp(pred_bboxes[:, 5], max=bdscan.bscan_shape[B][2]-1)
+            pred_bboxes[:, 3] = torch.clamp(pred_bboxes[:, 3], max=rpn_gt['scan_shape'][0]-1) # x max
+            pred_bboxes[:, 4] = torch.clamp(pred_bboxes[:, 4], max=rpn_gt['scan_shape'][1]-1) # y max
+            pred_bboxes[:, 5] = torch.clamp(pred_bboxes[:, 5], max=rpn_gt['scan_shape'][2]-1) # z max in voxel coords
 
             # easy lazy: TODO IMPROVE CLUSTERING (here later centroids collide)
             nms_filtered = nms3d(pred_bboxes.float(), pred_confs, 0.4)
@@ -116,7 +116,7 @@ class BSparseRPN_pure(nn.Module):
             #if min_size_filter.sum() == 0:
             #    min_size_filter[0] = True
 
-
+            # Breaks at size predicted boxes
             pred_bboxes = pred_bboxes[min_size_filter]
             pred_confs = pred_confs[min_size_filter]
 
@@ -131,12 +131,11 @@ class BSparseRPN_pure(nn.Module):
             brpn_conf.append(pred_conf)
 
             # find matching gt
-            if len(bdscan.bbboxes) > 0:
-                overlaps = bbox_overlaps(pred_bboxes.float(), bdscan.bbboxes[B].float())
+            if len(rpn_gt['bboxes']) > 0: # List with all bboxes in the scan
+                overlaps = bbox_overlaps(pred_bboxes.float(), rpn_gt['bboxes'].float())
                 if len(overlaps) >0:
-                    gt_target = [bdscan.bobj_inds[B][i] for i in torch.argmax(overlaps, 1)]
+                    gt_target = [rpn_gt['bobj_idxs'][i] for i in torch.argmax(overlaps, 1)]
                 else:
-                    #TODO IS THIS CORRECT???
                     gt_target = []
             else:
                 gt_target = len(pred_bboxes)*[0]
@@ -148,13 +147,11 @@ class BSparseRPN_pure(nn.Module):
         
 
     def training_step(self, x_e2: torch.Tensor, rpn_gt):
-        #with Timer('forward'):
+
         bpred_reg_values = self.forward(x_e2) # num occ x 8 featuredim
-        #with Timer('loss'):
         losses = self.loss(bpred_reg_values, rpn_gt)
-        #with Timer('to bboxes'):
         bpred_bboxes, bgt_target, brpn_conf = self._to_bboxes(bpred_reg_values, rpn_gt, self.conf['min_conf_train'], self.conf['max_proposals_train'])
-        # bdscan.bbboxes ~ bpred_bboxes
+
         return (bpred_bboxes, bgt_target, brpn_conf), losses, {}, {}
 
 
@@ -178,8 +175,6 @@ class BSparseRPN_pure(nn.Module):
 
             if (pos_mask).sum() == 0:
                 import pdb
-                print(bdscan.bseq_name)
-                print(bdscan.bscan_idx)
                 pdb.set_trace()
 
             # center coord, extent x,y,z similar to votenet

trainer.py
@@ -29,6 +29,7 @@ from baseconfig import CONF
 from model_cfg import init_cfg
 from utils.train_utils import sec_to_hm_str, _sparsify
 from utils.net_utils import vg_crop
+from datasets.sequence_chunking import chunk_sequence
 
 # Model import
 from models.BCompletionDec2 import BCompletionDec2
@@ -246,77 +247,18 @@ class Trainer:
         losses = dict()
         analyses = dict()
 
-        # BATCH INPUT PUT IN COLLATE FUNCTION, currently with chunking -------------------------------------------------
+        assert len(inputs) == 1
 
         for seq in inputs:
+            dense_features, sparse_features, rpn_features = chunk_sequence(seq, chunk_size=1, device=self.device)
 
-            chunk_size = 1
-            occ = []
-            coords = []
-            feats = []
-            sparse_features = []
-            dense_features = []
-            sparse_reg_features = []
+        sparse_reg_features = rpn_features[0]
+        sparse_box_features = rpn_features[1]
+        sparse_obj_features = rpn_features[2]
+        bscan_inst_mask = rpn_features[3]
+        bscan_obj_occs = rpn_features[4]
 
-            for idx, img in enumerate(seq):
-
-                # Sequence Level
-                img_grid = torch.unsqueeze(img['dense_grid'], dim=0)
-                occ.append(img_grid)
-                coords.append(img['sparse_coords'])
-                feats.append(img['sparse_feats'])
-
-                # Object level
-                reg_values = np.zeros([1, 7, img['dense_grid'].shape[0], img['dense_grid'].shape[1], img['dense_grid'].shape[2]])
-                scan_inst_mask = img['obj_scan_mask']
-
-                for obj in img['obj_anns']:
-
-                    box_3d = obj['box_3d']
-                    obj_idx = obj['instance_id']
-
-                    obj_scan_mask = scan_inst_mask == int(obj_idx)
-                    obj_scan_mask = obj_scan_mask.numpy()
-
-                    '''
-                    # NOT SURE IF NECESSARY
-                    obj_scan_mask_torch = obj_scan_mask.cuda()
-                    obj_scan_mask_torch2 = F.conv3d(
-                        obj_scan_mask_torch.float().unsqueeze(0).unsqueeze(0), weight=torch.ones(1, 1, 9, 9, 9).cuda(),
-                        padding=4
-                    )[0][0]
-                    obj_scan_mask = (obj_scan_mask_torch2 > 0).cpu().numpy()
-                    '''
-
-                    obj_scan_coords = np.argwhere(obj_scan_mask)
-
-                    obj_center = (box_3d[3:6] + box_3d[:3]) / 2
-                    obj_size = (box_3d[3:6] - box_3d[:3]) / 2
-
-                    delta_t = obj_center - obj_scan_coords
-                    delta_s = np.ones_like(delta_t) * obj_size
-                    w = 1 - (np.linalg.norm(delta_t / delta_s, axis=1, ord=2) / np.sqrt(3))
-
-                    reg_values[0, :, obj_scan_mask] = np.concatenate([np.expand_dims(w, 1), delta_t, delta_s], 1)
-
-                sparse_reg_features.append(reg_values)
-
-                if (idx+1) % chunk_size == 0:
-                    bcoords = ME.utils.batched_coordinates(coords)
-                    bfeats = torch.from_numpy(np.concatenate(feats, 0)).float()
-                    sparse_chunk = ME.SparseTensor(bfeats.to(self.device),
-                                                      bcoords.to(self.device))  # Sparse tensor expects batched data
-                    #dense_chunk = sparse_chunk.dense(min_coordinate=self.min_coords)
-                    dense_chunk = torch.unsqueeze(torch.cat(occ, dim=0), dim=1).to(self.device)
-                    coords = []
-                    feats = []
-                    occ = []
-                    sparse_features.append(sparse_chunk)
-                    dense_features.append(dense_chunk)
-
-        # --------------------------------------------------------------------------------------------------------------
-
-        #coords, feats = sparse_features.decomposed_coordinates_and_features # for decomposition of batched data
+        #Todo: Adapt to loop per chunk
         dense_features = dense_features[0]
         sparse_features = sparse_features[0]
         sparse_reg_features = torch.from_numpy(sparse_reg_features[0]) # B x 7 x W x L x H
@@ -333,6 +275,11 @@ class Trainer:
 
         rpn_gt = {}
         rpn_gt['breg_sparse'] = sparse_reg_tensor
+        rpn_gt['scan_shape'] = dense_features.shape[2:]
+        rpn_gt['bboxes'] = torch.from_numpy(np.concatenate(sparse_box_features[0], axis=0)).to(self.device)
+        rpn_gt['bobj_idxs'] = sparse_obj_features[0]
+        rpn_gt['bscan_inst_mask'] = bscan_inst_mask
+
 
         # Dense Pipeline ----------------------------------------------------------------------------------------------
         x_e1, x_e2, x_d2 = self.backbone.training_step(dense_features)  # enc_layer_1, enc_layer_2, dec_layer_2
@@ -348,9 +295,9 @@ class Trainer:
         btarget_occ, bbbox_lvl0_compl, bgt_target_compl = [], [], []
         btarget_noc = []
         for B, (bbox_lvl0, gt_target) in enumerate(zip(bbbox_lvl0, bgt_target)):
-            inst_crops = vg_crop(bdscan.bscan_inst_mask[B], bbox_lvl0) # BSCAN INST MASK IN 3D? or just box and crop unoccupied
+            inst_crops = vg_crop(bscan_inst_mask[B], bbox_lvl0) # BSCAN INST MASK IN 3D? or just box and crop unoccupied
 
-            target_num_occs = [(torch.abs(bdscan.bobjects[B][obj_idx].sdf) < 2).sum() for obj_idx in gt_target]
+            target_num_occs = [(bscan_obj_occs[B][str(obj_idx)] != 0).sum() for obj_idx in gt_target] # number of occupancies per object
             inst_occ_targets = [(inst_crop == int(obj_idx)).unsqueeze(0) for obj_idx, inst_crop in
                                 zip(gt_target, inst_crops)]
             valid_inst_occ_target = []
@@ -378,14 +325,14 @@ class Trainer:
 
         # Completion
         completion_output, completion_losses, completion_analyses, completion_timings = self.completion.training_step(
-            x_d2, x_e2, x_e1, bdscan, bbbox_lvl0_compl_s, bgt_target_compl
+            x_d2, x_e2, x_e1, rpn_gt, bbbox_lvl0_compl_s, bgt_target_compl
         )
         losses["completion"] = completion_losses
         total_loss += torch.mean(torch.cat(completion_losses["bweighted_loss"], 0))
 
         # Nocs
         noc_output, noc_losses, noc_analyses, noc_timings = self.noc.training_step(
-            x_d2, x_e2, x_e1, bdscan, bbbox_lvl0_compl_s, bgt_target_compl
+            x_d2, x_e2, x_e1, rpn_gt, bbbox_lvl0_compl_s, bgt_target_compl
         )
         losses["noc"] = noc_losses
         analyses["noc"] = noc_analyses

utils/pose_utils.py
@@ -1,24 +1,87 @@
 import numpy as np
 import open3d as o3d
 import mathutils
+from sklearn.preprocessing import minmax_scale
+
+def occ2noc2(voxel_grid, euler_rot):
+    '''
+    Calculate nocs map from occupancy grid and 3D rot
+    Carefull CAD space is moved up in z axis when rotation for nocs space might result in an issue
+    '''
+
+    euler = mathutils.Euler(euler_rot)
+    rot = np.array(euler.to_matrix())
+
+    shape = voxel_grid.shape
+    max_dim = np.array(shape).max() - 1 # idx starts at 0
+
+    nonzero_inds = np.nonzero(voxel_grid)
+
+    points = nonzero_inds / max_dim  # norm 0 - 1
+    points = points.numpy()
+
+
+
+    world_pc = rot @ points.transpose()
+    world_pc = world_pc.transpose()
+
+    return world_pc
 
 def occ2noc(voxel_grid, euler_rot):
     '''
     Calculate nocs map from occupancy grid and 3D rot
+    euler rotation: CAD2World space in Blender coord space
     '''
+
+    def center(points):
+        x_range = points[:,0].max() - points[:,0].min()
+        y_range = points[:,1].max() - points[:,1].min()
+        z_range = points[:,2].max() - points[:,2].min()
+        ranges = np.array([x_range, y_range, z_range])
+        return points - ranges/2
 
     euler = mathutils.Euler(euler_rot)
     rot = np.array(euler.to_matrix())
 
     nonzero_inds = np.nonzero(voxel_grid)[:-1]
 
-    points = nonzero_inds / 31 - 0.5 # norm + center -0.5 to 0.5
+    points = nonzero_inds / 31 - 0.5
     points = points.numpy()
+    points[:, 1] -= points[:, 1].min() # CAD space y is shifted up to start at 0
 
-    world_pc = rot @ points.transpose()
-    world_pc = world_pc.transpose()
+    '''
+    nocs_pcd = o3d.geometry.PointCloud()
+    nocs_pcd.points = o3d.utility.Vector3dVector(points)
+    nocs_origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+    o3d.visualization.draw_geometries([nocs_pcd, nocs_origin])
+    '''
 
-    return world_pc
+    noc_pc = rot @ points.transpose()
+    noc_pc = noc_pc.transpose() + 0.5 # NOC space
+
+    return noc_pc
+
+def discretize_noc(noc_pts, bbox, quantization_size=0.03):
+    '''
+    Discretize nocs pointcloud and squash between object box coordinates
+    Maybe issue since box is axis aligned
+    '''
+
+    scaling = 1 / quantization_size
+    x_min, x_max = bbox[0], bbox[3]
+    y_min, y_max = bbox[1], bbox[4]
+    z_min, z_max = bbox[2], bbox[5]
+
+    noc_discretized = np.rint(noc_pts * scaling)
+    x_scaled = minmax_scale(noc_discretized[:,0], feature_range=(x_min, x_max))
+    y_scaled = minmax_scale(noc_discretized[:,1], feature_range=(y_min, y_max))
+    z_scaled = minmax_scale(noc_discretized[:,2], feature_range=(z_min, z_max))
+
+    noc_discretized = np.array([x_scaled, y_scaled, z_scaled]).T
+    test = noc_discretized
+
+    return noc_discretized
+
 
 
 def backproject_rgb(rgb, depth, intrinsics, debug_mode=False):