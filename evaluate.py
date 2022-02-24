fix nocs rotation matrix, further tracking

evaluate.py
@@ -107,8 +107,10 @@ def evaluate_bdscan(outputs, inputs, losses=None, analyses=None):
             info_df['prop_idx'] = [j]
             info_df['gt_target'] = [obj_idx]
             info_df['pred_aligned2scan'] = [outputs['noc']['pred_aligned2scans'][B][j].numpy()]
+            info_df['occ'] = [torch.squeeze(outputs['completion'][B][j]).detach().cpu().numpy()]
+            info_df['noc'] = [torch.squeeze(outputs['noc']['noc_values'][B][j]).detach().cpu().numpy()]
+            info_df['bbox'] = [outputs['rpn']['bbbox_lvl0'][B][j].detach().cpu().numpy()]
 
-            
             #info_df['model_id'] = [dobject.model_id]
             info_df['class_name'] = [dobject['class_name']]
             #info_df['scan_coverage_amodal'] = [dobject.scan_coverage_amodal]
@@ -120,8 +122,8 @@ def evaluate_bdscan(outputs, inputs, losses=None, analyses=None):
                 # TODO MORE ADVANCED EVALUATION
                 gt_bbox = torch.from_numpy(dobject['box_3d']).cuda().int()
 
-                rpn_df['iou'] = [iou3d(pred_bbox.unsqueeze(0).float(), gt_bbox.unsqueeze(0).float()).item()]
-                rpn_df['conf'] = [rpn_conf[j].item()]
+                rpn_df['rpn_iou'] = [iou3d(pred_bbox.unsqueeze(0).float(), gt_bbox.unsqueeze(0).float()).item()]
+                rpn_df['rpn_conf'] = [rpn_conf[j].item()]
 
                 '''
                 if 'rpn' in losses:
@@ -146,7 +148,7 @@ def evaluate_bdscan(outputs, inputs, losses=None, analyses=None):
             #gt_seen_compl_crop = (torch.abs(vg_crop(dscan.tsdf_geo, pred_bbox, crop_box=True )) <= 0.4) & gt_scan_compl_crop
             #pred_seen_compl_crop = (torch.abs(vg_crop(dscan.tsdf_geo, pred_bbox, crop_box=True )) <= 0.4) & pred_compl_crop
 
-            compl_df['iou'] = [mask_iou(pred_compl_crop, gt_scan_compl_crop, 1)]
+            compl_df['compl_iou'] = [mask_iou(pred_compl_crop, gt_scan_compl_crop, 1)]
             compl_df['unseen_iou'] = [mask_iou(pred_unseen_compl_crop, gt_unseen_compl_crop, 1)]
             #compl_df['seen_iou'] = [mask_iou(pred_seen_compl_crop, gt_seen_compl_crop, 1)]
 

tracking/tracking_fcts.py
@@ -4,8 +4,6 @@ from trimesh.transformations import translation_matrix, rotation_matrix, scale_a
 import motmetrics as mm
 import pandas as pd
 
-
-
 #### helper
 
 def get_moving_obj(gt_dscan_i):
@@ -28,8 +26,6 @@ def voxelize_unit_pc(pc, shape=20):
     vg = np.zeros([shape, shape, shape]).astype(bool)
     vg[tuple(indices.T)] = True
     return vg
-
-
 
 def vg_crop(vg, bboxes, spatial_end=True, crop_box=False):
     # vg: ... X W X L X H
@@ -137,7 +133,7 @@ def pred_trajectory(trajectories, dscan_j, cam_grid2cam_free, obj0, traj_crit='w
                     if int(dscan_j.scan_idx) - int(traj[-1]['scan_idx']) < 10:
                         # last hypo of this trajectory is tempory close
                         prev_obj = traj[-1]['obj']
-                        prev_prop_matrix[traj_idx, obj_idx] = np.linalg.norm( (cam_grid2cam_free @ prev_obj.aligned2scan)[:3,3]-  (cam_grid2cam_free @ obj_j.aligned2scan)[:3,3])
+                        prev_prop_matrix[traj_idx, obj_idx] = np.linalg.norm( (cam_grid2cam_free @ prev_obj.aligned2scan)[:3,3]-  (cam_grid2cam_free @ obj_j.aligned2scan)[:3,3]) # Scan2World @ Cad2scan = cad2world
 
 
     return trajectories
@@ -165,7 +161,7 @@ def analyse_trajectories(seq_name, match_criterion ):
         pred_dscan_i = list(pred_dseq.scans.values())[0]
 
         cam_free2world_free = np.array(gt_dscan_i.camera_pose) @ reflection_matrix([0, 0, 0], [0, 0, 1]) # Cam2world
-        cam_grid2cam_free = np.linalg.inv(cam_free2world_free) @ gt_dscan_i.scan2world # maybe discretized to free
+        cam_grid2cam_free = np.linalg.inv(cam_free2world_free) @ gt_dscan_i.scan2world # maybe discretized to free        #World2Cam Scan2World = Scan2Cam
 
         gt_target= get_moving_obj(gt_dscan_i)
         seq_data[scan_idx] = {'cam_free2world_free': cam_free2world_free,

tracking/tracking_front.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 from trimesh.transformations import translation_matrix, rotation_matrix, scale_and_translate, scale_matrix, reflection_matrix
-#import motmetrics as mm
+import motmetrics as mm
 import pandas as pd
 
 from utils.net_utils import vg_crop
@@ -27,24 +27,24 @@ class Tracker:
             gt_scan = gt_seq_df.loc[gt_seq_df['scan_idx'] == scan_idx]
             pred_scan = pred_seq_df.loc[pred_seq_df['scan_idx'] == scan_idx]
 
-            cam_free2world_free = gt_scan.loc[0, 'campose'] @ reflection_matrix([0, 0, 0], [0, 0, 1]) # Cam2world
+            gt_scan_dct = gt_scan.to_dict(orient='index')  # idxs, clmn
+            pred_scan_dct = pred_scan.to_dict(orient='index')
+
+            cam_free2world_free = list(gt_scan_dct.values())[0]['campose'] @ reflection_matrix([0, 0, 0], [0, 0, 1]) # Cam2world
             cam_grid2cam_free = np.linalg.inv(cam_free2world_free) #@ gt_dscan_i.scan2world # maybe discretized to free
+            cam_grid2cam_free[:3,3] *= 0.04 #transfrom from discretized to coords
 
             gt_target = 0
             seq_data[scan_idx] = {'cam_free2world_free': cam_free2world_free,
                                   'cam_grid2cam_free': cam_grid2cam_free,
                                   'gt_target': gt_target}
 
-            gt_scan_dct = gt_scan.to_dict(orient='index') #idxs, clmn
-            pred_scan_dct = pred_scan.to_dict(orient='index')
-            #pred_dscan_i.tsdf_geo = gt_dscan_i.tsdf_geo
-
             # Initialize trajectory
             if scan_idx == 0:
                 for obj in pred_scan_dct.values():
                     has_similar = False
                     for pred_traj in pred_trajectories: # Cad to scan
-                        if np.linalg.norm(pred_traj[0]['obj'].aligned2scan[:3, 3] - obj['aligned2scan'][:3, 3]) < 0.6 / 0.04: # 0.6m / 0.03 = quantization size?
+                        if np.linalg.norm(pred_traj[0]['obj']['pred_aligned2scan'][:3, 3] - obj['pred_aligned2scan'][:3, 3]) < 0.6 / 0.04: # 0.6m / 0.03 = quantization size?
                             has_similar = True
                     if not has_similar: # not an object which is close
                         pred_trajectories.append([{'obj':obj, 'scan_idx':obj['scan_idx']}])
@@ -55,67 +55,132 @@ class Tracker:
                 pred_trajectories = self.pred_trajectory(pred_trajectories, pred_scan_dct, cam_grid2cam_free, None, occ_grid=occ_grid, traj_crit='with_first', match_criterion=self.match_criterion)
                 for gt_traj in gt_trajectories:
                     for gt_obj in gt_scan_dct.values():
-                        if gt_traj[0]['obj'].obj_idx == gt_obj.obj_idx:
+                        if gt_traj[0]['obj']['obj_idx'] == gt_obj['obj_idx']:
                             gt_traj.append({'obj': gt_obj, 'scan_idx': gt_obj['scan_idx']}) # build list per trajectory
                             break
 
         return pred_trajectories, gt_trajectories, seq_data
 
     def pred_trajectory(self, trajectories, dscan_j, cam_grid2cam_free, obj0, occ_grid=None, traj_crit='with_first', match_criterion='iou0'):
+
         # assign proposals to trajectories based on traj_crit and match_criterion
-        traj_prop_matrix = np.zeros((len(trajectories), len(dscan_j.objects)))
-        prev_prop_matrix = 1000 * np.ones((len(trajectories), len(dscan_j.objects)))
-        proposal_ids = list(dscan_j.objects.keys())
-        for obj_idx, (obj_id_j, obj_j) in enumerate(dscan_j.objects.items()):
+        traj_prop_matrix = np.zeros((len(trajectories), len(dscan_j.values())))
+        prev_prop_matrix = 1000 * np.ones((len(trajectories), len(dscan_j.values())))
+        obj_idx = 0
+        for _, obj_j in dscan_j.items():
             if traj_crit == 'with_first':
                 if match_criterion == 'iou0' or match_criterion == 'iou0_segm':
-                    if not hasattr(obj_j, 'obj_j_occ_in_noc'):
-                        if obj_j.sdf is not None:
-                            # is gt
-                            obj_j.occ = obj_j.sdf < 2
-                            obj_j_occ_in_noc = self.dot(np.linalg.inv(obj_j.noc2bbox_amodal),
-                                                   np.argwhere(obj_j.occ))  # bbox to nocs
-                        else:
-                            if match_criterion == 'iou0_segm':
-                                surf_occ = (vg_crop((occ_grid > 0), obj_j.bbox) & (obj_j.occ > 0))
-                                obj_j_occ_in_noc = obj_j.noc[:, surf_occ].T
-                            else:
-                                obj_j_occ_in_noc = obj_j.noc[:, obj_j.occ > 0].T
-                        obj_j.occ_in_noc = obj_j_occ_in_noc
+                    surf_occ = (vg_crop((occ_grid > 0), obj_j['bbox']) & (obj_j['occ'] > 0))
+                    obj_j_occ_in_noc = obj_j['noc'][:, surf_occ].T
 
-                    obj_j_noc_vg = self.voxelize_unit_pc(obj_j.occ_in_noc)  # voxelized pc
-                    best_traj_idx, best_iou = -1, 0
+                    obj_j['occ_in_noc'] = obj_j_occ_in_noc
+                    obj_j_noc_vg = self.voxelize_unit_pc(obj_j_occ_in_noc)  # voxelized pc
+
                     for traj_idx, traj in enumerate(trajectories):
                         start_obj = traj[0]['obj']
 
-                        if not hasattr(start_obj, 'occ_in_noc'):
-                            if obj_j.sdf is not None:
-                                # is gt
-                                start_obj.occ = start_obj.sdf < 2
-                                start_obj_j_occ_in_noc = self.dot(np.linalg.inv(start_obj.noc2bbox_amodal),
-                                                             np.argwhere(start_obj.occ))
-                            else:
-                                if match_criterion == 'iou0_segm':
-                                    surf_occ = (vg_crop((occ_grid > 0), start_obj.bbox) & (
-                                                start_obj.occ > 0))
-                                    start_obj_j_occ_in_noc = start_obj.noc[:, surf_occ].T
-                                else:
-                                    start_obj_j_occ_in_noc = start_obj.noc[:, start_obj.occ > 0].T
+                        if match_criterion == 'iou0':
+                            surf_occ = (vg_crop((occ_grid > 0), start_obj['bbox']) & (
+                                        start_obj['occ'] > 0))
+                            start_obj_j_occ_in_noc = start_obj['noc'][:, surf_occ].T
 
-                            start_obj.occ_in_noc = start_obj_j_occ_in_noc
+                            start_obj['occ_in_noc'] = start_obj_j_occ_in_noc
 
-                        start_obj_noc_vg = self.voxelize_unit_pc(start_obj.occ_in_noc)
+                        start_obj_noc_vg = self.voxelize_unit_pc(start_obj_j_occ_in_noc)
 
                         iou3d = float((obj_j_noc_vg & start_obj_noc_vg).sum()) / (obj_j_noc_vg | start_obj_noc_vg).sum()
                         traj_prop_matrix[traj_idx, obj_idx] = iou3d
 
-                        if int(dscan_j.scan_idx) - int(traj[-1]['scan_idx']) < 10:
+                        if int(list(dscan_j.values())[0]['scan_idx']) - int(traj[-1]['scan_idx']) < 10:
                             # last hypo of this trajectory is tempory close
                             prev_obj = traj[-1]['obj']
                             prev_prop_matrix[traj_idx, obj_idx] = np.linalg.norm(
-                                (cam_grid2cam_free @ prev_obj.aligned2scan)[:3, 3] - (cam_grid2cam_free @ obj_j.aligned2scan)[:3, 3])
+                                (cam_grid2cam_free @ prev_obj['pred_aligned2scan'])[:3, 3] - (cam_grid2cam_free @ obj_j['pred_aligned2scan'])[:3, 3])
 
+            obj_idx += 1
         return trajectories
+
+    def get_traj_table(self, traj, seq_data, traj_id):
+        traj_df = pd.DataFrame()
+
+        for k in range(len(traj)):
+            scan_idx = traj[k]['scan_idx']
+            if 'gt' in traj_id:
+                aligned2cam_free = seq_data[scan_idx]['cam_grid2cam_free'] @ traj[k]['obj']['aligned2scan']
+            else:
+                aligned2cam_free = seq_data[scan_idx]['cam_grid2cam_free'] @ traj[k]['obj']['pred_aligned2scan']
+
+            aligned2world_free = seq_data[scan_idx]['cam_free2world_free'] @ aligned2cam_free
+            cam_t = aligned2cam_free[:3, 3]
+            world_t = aligned2world_free[:3, 3]
+
+            single_df = pd.DataFrame(dict(scan_idx=scan_idx,
+                                          traj_id=traj_id,
+                                          cam_x=cam_t[0],
+                                          cam_y=cam_t[1],
+                                          cam_z=cam_t[2],
+                                          world_x=world_t[0],
+                                          world_y=world_t[1],
+                                          world_z=world_t[2],
+                                          obj_idx=traj[k]['obj']['obj_idx'],
+                                          ref_obj_idx=traj[k]['ref_obj_idx'] if 'ref_obj_idx' in traj[k] else None,
+                                          gt_obj_idx=seq_data[scan_idx]['gt_target'] if seq_data[scan_idx][
+                                                                                                    'gt_target'] is not None else None,
+                                          ), index=[scan_idx]
+                                     )
+
+            traj_df = pd.concat([traj_df, single_df], axis=0)
+        return traj_df
+
+    def get_traj_tables(self, trajectories, seq_data, prefix):
+        traj_tables = pd.DataFrame()
+        for t in range(len(trajectories)):
+            traj_table = self.get_traj_table(trajectories[t], seq_data, f'{prefix}_{t}')
+            traj_tables = pd.concat([traj_tables, traj_table], axis=0)
+        return traj_tables
+
+    def eval_mota(self, pred_table, mov_obj_traj_table):
+        # compute mota based on l2_th
+        l2_th = 0.25
+
+        mh = mm.metrics.create()
+        all_traj_summary = pd.DataFrame()
+
+        for pred_traj_id in pred_table['traj_id'].drop_duplicates():
+            acc = mm.MOTAccumulator(auto_id=True)
+            for scan_idx in mov_obj_traj_table['scan_idx']:
+                gt_cam = np.array(
+                    mov_obj_traj_table[mov_obj_traj_table['scan_idx'] == scan_idx][['cam_x', 'cam_y', 'cam_z']])
+                # get gt position in camera frame
+                gt_idx = int(mov_obj_traj_table[mov_obj_traj_table['scan_idx'] == scan_idx]['obj_idx'])
+
+                gt_objects = [gt_idx]
+
+                hypo_table = pred_table[(pred_table['scan_idx'] == scan_idx) & (pred_table['traj_id'] == pred_traj_id)]
+                pred_objects = []
+                dist_matrix = np.nan * np.ones((len(gt_objects), len(hypo_table)))
+                # print(dist_matrix.shape)
+                for j, hypo in enumerate(hypo_table.iterrows()):
+                    hypo_cam = np.array(hypo[1][['cam_x', 'cam_y', 'cam_z']])
+                    # get hypo position in camera frame
+                    hypo_id = int(hypo[1]['traj_id'].split('_')[-1])  # format was pred_X
+                    pred_objects.append(hypo_id)
+                    for i, gt_obj in enumerate(gt_objects):
+                        # SINGLE GT OBJECT THOUGH
+                        dist_matrix[i][j] = mm.distances.norm2squared_matrix(gt_cam, hypo_cam, max_d2=l2_th)
+                        # l2 distance between gt object and hypothesis, capped to l2_th
+
+                acc.update(
+                    gt_objects,  # Ground truth objects in this frame
+                    pred_objects,  # Detector hypotheses in this frame
+                    dist_matrix
+                )
+
+            summary = mh.compute(acc, metrics=['num_frames', 'mota', 'motp', 'num_matches', 'num_misses',
+                                               'num_false_positives'], name='acc')
+            summary['traj_id'] = pred_traj_id
+            all_traj_summary = pd.concat([all_traj_summary, summary])
+        return all_traj_summary
 
     def dot(self, transform, points):
         if type(points) == list:

trainer.py
@@ -64,6 +64,7 @@ class Trainer:
         cfg = init_cfg()
         self.sparse_pretrain_ep = cfg['general']['sparse_pretrain_epochs']
         self.dense_pretrain_ep = cfg['general']['dense_pretrain_epochs']
+        self.overfit = cfg['general']['overfit']
 
         # Sparse Backbone & RPN
         self.models["sparse_backbone"] = networks.PureSparseBackboneCol_Res1(conf=cfg['sparse_backbone'])
@@ -112,7 +113,7 @@ class Trainer:
         DATA_DIR = CONF.PATH.FRONTDATA
         self.dataset = datasets.Front_dataset
 
-        train_dataset = self.dataset(base_dir=DATA_DIR, split='train')
+        train_dataset = self.dataset(base_dir=DATA_DIR, split='train', overfit=self.overfit)
 
         self.train_loader = DataLoader(
             train_dataset,
@@ -240,7 +241,7 @@ class Trainer:
 
         self.set_train()
 
-    def inference(self, store_results=False, merge_seq=False, vis=False):
+    def inference(self, store_results=False, vis=False):
         """
         Run the entire inference pipeline and perform tracking afterwards
         """
@@ -252,8 +253,6 @@ class Trainer:
         collection_eval_df = pd.DataFrame()
         collection_gt_eval_df = pd.DataFrame()
 
-        gt_seqs = dict()
-        pred_seqs = dict()
         occ_grids = dict()
 
         for batch_idx, inputs in enumerate(self.val_loader):
@@ -272,11 +271,11 @@ class Trainer:
                                                                 ignore_index=True)
 
                 # Scan level GT occupancy grids
-                for B, grid in inputs[0]:
+                for B, grid in enumerate(inputs[0]):
                     seq_pattern = "val/(.*?)/coco_data"
                     seq_name = re.search(seq_pattern, bscan_info[B]).group(1)
                     if seq_name not in occ_grids:
-                        grid = torch.squeeze(grid)
+                        grid = torch.squeeze(grid).detach().cpu().numpy()
                         occ_grids[seq_name] = grid
 
         # Sort and rearrange df
@@ -290,9 +289,9 @@ class Trainer:
             gt_seq_df = collection_gt_eval_df.loc[collection_gt_eval_df['seq_name'] == seq]
             pred_seq_df = collection_eval_df.loc[collection_eval_df['seq_name'] == seq]
             pred_trajectories, gt_trajectories, seq_data = self.Tracker.analyse_trajectories(gt_seq_df, pred_seq_df, occ_grids[seq])
-
-
-
+            gt_traj_tables = self.Tracker.get_traj_tables(gt_trajectories, seq_data, 'gt')
+            pred_traj_tables = self.Tracker.get_traj_tables(pred_trajectories, seq_data, 'pred')
+            seq_mota_summary = self.Tracker.eval_mota(pred_traj_tables, gt_traj_tables)
 
         if store_results:
             # store evaluations
@@ -301,9 +300,6 @@ class Trainer:
 
             collection_eval_df.to_csv(Path(eval_dir, 'collection.csv'), index=False)
             collection_gt_eval_df.to_csv(Path(eval_dir, 'collection_gt.csv'), index=False)
-
-        if merge_seq:
-            merge2seq(self.experiment_dir, "inference")
 
     def run_epoch(self, sparse_pretrain=False, dense_pretrain=False):
         self.set_train()

utils/pose_utils.py
@@ -17,7 +17,7 @@ def get_noc2scan(rot_3d, loc_3d, scale, bin_vox, quantization_size=0.04):
     euler = mathutils.Euler(rot_3d)
     rot = np.array(euler.to_matrix())
 
-    # Cad2Scan
+    # Cad2Scan # Cad2World
     scale_quant = 1/quantization_size
     cad2scan = np.identity(4)
     cad2scan[:3, :3] = rot
@@ -36,7 +36,7 @@ def get_noc2scan(rot_3d, loc_3d, scale, bin_vox, quantization_size=0.04):
 
     cad2noc = np.linalg.inv(noc2cad)
 
-    noc2scan = noc2cad @ cad2scan
+    noc2scan = cad2scan @ noc2cad # cad2scan @ noc2cad = noc2scan?? -> lower with now
 
     return noc2scan, cad2noc
 